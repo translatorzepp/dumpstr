7e3a033096d15b20ebb5408af8cb1b1644bf1f57
zpalmer <zoe.palmer@getbraintree.com>
2018-04-13 19:13:30 -0500

    Rework TumblrGateway as a post-getter

--- app/models/tumblr_gateway.rb
+++ app/models/tumblr_gateway.rb
@@ -10,29 +10,24 @@ class TumblrGateway
     @client = Tumblr::Client.new
   end
 
-  def likes_count(blog_to_search)
-    response = @client.blog_likes(blog_to_search, {:limit => 1})
-    # error handling around status
-    response["liked_count"]
-  end
+  def all_liked_posts(blog_to_search)
+    result = {}
 
+    # if request returns 200/201, it'll only return the response from the body
+    #   otherwise, it'll return the 'meta' (incl status, msg) + body if it exists
+    # will accept a max limit of 50. TODO: look at using offsets to get everything
+    response_from_tumblr = @client.blog_likes(blog_to_search, {:limit => 50})
 
-  def search_likes(blog_to_search:, post_text:, post_creator: nil)
-    match_regex = Regexp.new(post_text, Regexp::IGNORECASE)
+    if response_from_tumblr["status"]
+      result["liked_posts"] = []
+      result["message"] = response_from_tumblr["msg"]
 
-    limit = likes_count(blog_to_search)
-    like_response = @client.blog_likes(blog_to_search, {:limit => limit})
-    status = like_response["status"]
-    Rails.logger.debug("tumblr client search response status: #{status}")
-    # error handling around status
-
-    liked_posts = like_response["liked_posts"] #array of hashes
-
-    liked_posts.each_with_object({}) do |post, matching_posts|
-      if post["summary"].match(match_regex) || post["tags"].any? { |tag| tag.match(match_regex) }
-        matching_posts[post["post_url"]] = post["summary"]
-      end
+      Rails.logger.error("Tumblr client error. Status Code #{response_from_tumblr['status']}. Message: #{response_from_tumblr['msg']}")
+    else
+      result["liked_posts"] = response_from_tumblr["liked_posts"]
+      result["message"] = nil
     end
+
+    result
   end
 end
-
---
+++ spec/models/tumblr_gateway_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "TumblrGateway" do
+
+  let(:successful_client_response) { {"liked_posts" => [{"summary" => "a post"}], "liked_count" => 1} }
+  let(:failure_client_response) { {"status"=>404, "msg"=>"Not Found"} }
+
+  let(:gateway) { TumblrGateway.new }
+
+  describe "all_liked_posts" do
+    before :each do
+      @client_double = instance_double("Tumblr::Client")
+      allow(Tumblr::Client).to receive(:new).and_return(@client_double)
+
+      allow(@client_double).to receive(:blog_likes).with("existing", anything).and_return(successful_client_response)
+      allow(@client_double).to receive(:blog_likes).with("unexisting", anything).and_return(failure_client_response)
+    end
+
+    it "on success, returns a hash with a collection of liked posts and a nil message" do
+      success_result = gateway.all_liked_posts("existing")
+      expect(success_result).to eq({"liked_posts" => [{"summary" => "a post"}], "message" => nil})
+    end
+
+    it "on failure, returns a hash with an empty collection of liked posts and an error message" do
+      failure_result = gateway.all_liked_posts("unexisting")
+      expect(failure_result).to eq({"liked_posts" => [], "message" => "Not Found"})
+    end
+
+    it "logs on failure" do
+      expect(Rails.logger).to receive(:error) do |log|
+        expect(log).to include("Tumblr client error")
+        expect(log).to include("404")
+        expect(log).to include("Not Found")
+      end
+
+      failure_result = gateway.all_liked_posts("unexisting")
+    end
+  end
+end
