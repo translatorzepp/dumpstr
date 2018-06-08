f04475839010173eb31e296ba2401360247170dd
zpalmer <zoe.palmer@getbraintree.com>
2018-06-08 15:08:17 -0500

    Show results as links with summary info

--- app/controllers/search_likes_controller.rb
+++ app/controllers/search_likes_controller.rb
@@ -7,17 +7,25 @@ class SearchLikesController < ApplicationController
   end
 
   def results
+    @results = []
     search_params = params.extract!(:blog_name, :search_text, :source_blog)
     tumbler = TumblrSearch.new(blog_to_search: search_params[:blog_name])
     results = tumbler.find_liked_posts_matching(post_text: search_params[:search_text], post_creator: search_params[:source_blog])
+
     # flash results[:error_message]
-    @results = results[:posts]
     @result_count = results[:posts].size
-    display_results
+    display_results(results[:posts])
   end
 
-  def display_results
-    # any necessary parsing to pass it forward here?
+  def display_results(posts)
+    display_friendly_links = posts.each_with_object([]) do |post, display_friendly_links|
+      display_friendly_links << {
+        :text => "liked from #{post['blog_name']}: #{post['summary']}",
+        :url => post["post_url"]
+      }
+    end
+
+    @results = display_friendly_links
   end
 
   def santize_search_terms
