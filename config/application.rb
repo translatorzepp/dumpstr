407bedfac7cb017b10fa83cbc5f9f311de808afe
zpalmer <zoe.palmer@getbraintree.com>
2018-06-20 17:01:13 -0700

    No database

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
 require_relative 'boot'
 
-require 'rails/all'
+# No database: replacing require 'rails/all' with specifics
+require 'active_model/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
